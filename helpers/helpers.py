1a1883fd487e1f18b6742c6460f392fb746ade8d
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-02-14 17:22:38 +0100

    basic logger init added

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,4 +1,7 @@
+import sys
+
 from selenium import webdriver
+from loguru import logger
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -7,3 +10,7 @@ def get_driver():
     return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 
+def init_logger():
+    logger.remove(0)
+    logger.add(sys.stderr, level="INFO", colorize=True,
+               format="<black><yellow>[{level}]</yellow>: <green>{message}</green> @ {time:HH:mm:ss.SS}</black>")
