920e657dfc3da67de5e2342c963a40c47f04a27b
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-02-14 12:19:21 +0100

    logic for accepting the cookies form implemented

--- core.py
+++ core.py
@@ -1,8 +1,21 @@
+import constants
+from forms.cookies import CookiesForm
+from forms.pages.home import HomePage
 from helpers import get_driver
 
 
 def main():
     driver = get_driver()
+    try:
+        driver.get(constants.BASE_URL)
+        home_page = HomePage(driver)
+
+        if home_page.is_cookies_form_displayed():
+            cookies_form = CookiesForm(driver)
+            cookies_form.accept_cookies()
+
+    finally:
+        driver.quit()
 
 
 if __name__ == "__main__":
